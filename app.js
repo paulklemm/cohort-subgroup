fbc5efa7c65f1366da239c8315ab357346d3aef4
Lena Cibulski <lena.cibulski@st.ovgu.de>
2015-05-17 12:48:08 +0200

    Added dynamic list for subgroups

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ app.run(['$rootScope', 'data', function($rootScope, data){
   d3.csv('data/breast_fat_labels.csv', function(result){ data.dataset = result; $rootScope.$broadcast("dataLoaded"); });
   d3.json('data/dictionary_new_names.json', function(result){ data.jsondata = result; });
 }]);
+
+
+
+app.controller('ListCtrl', ['$scope', function($scope) {
+  var i = 0;
+  $scope.extendSubgroupList = function() {
+    i += 1;
+    console.log('extend');
+    var list = d3.select("#subgroupList")
+      .append("li")
+      .text("Subgruppe " + i);
+  };
+}]);
